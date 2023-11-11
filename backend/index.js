7bffb753ecd0946167f4aefd25f0c6013868cfc6
itzhimangshu <itzhimangshu@gmail.com>
2023-11-11 19:51:53 +0530

    create our first http route

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ dotenv.config();
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.status(234).send('Welcome to MERN Stack');
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`server is running at ${process.env.PORT} `);
 })
