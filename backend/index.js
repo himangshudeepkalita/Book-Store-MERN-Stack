167f2e0ce64ab328bdf73e16b781fce0431c6621
itzhimangshu <itzhimangshu@gmail.com>
2023-11-13 16:52:50 +0530

    CORS policy in node js and express js

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,25 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import bookRoute from './routes/book.route.js';
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: All All Origins with Default of cors(*)
+// app.use(cors());
+// Option 2: Allow Custom Origins
+app.use(
+    cors({
+        origin: 'http://127.0.0.1:5173/',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    })
+)
 
 app.get('/', (req, res) => {
     res.status(234).send('Welcome to MERN Stack');
