2065ebc7e011608f852951cc131630158c2fa7c2
itzhimangshu <itzhimangshu@gmail.com>
2023-11-13 17:50:22 +0530

    show books list in react

--- backend/index.js
+++ backend/index.js
@@ -12,15 +12,15 @@ app.use(express.json());
 
 // Middleware for handling CORS POLICY
 // Option 1: All All Origins with Default of cors(*)
-// app.use(cors());
+app.use(cors());
 // Option 2: Allow Custom Origins
-app.use(
-    cors({
-        origin: 'http://127.0.0.1:5173/',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type']
-    })
-)
+// app.use(
+//     cors({
+//         origin: 'http://127.0.0.1:5173/',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type']
+//     })
+// )
 
 app.get('/', (req, res) => {
     res.status(234).send('Welcome to MERN Stack');
